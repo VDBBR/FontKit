7c42588c4c63a38f1c623741aae8d5d9e79b21e4
Daniel Saidi <daniel.saidi@gmail.com>
2025-07-08 10:00:41 +0200

    Add package with OpenDyslexic font

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "FontKit",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+        .tvOS(.v15),
+        .watchOS(.v8),
+        .macOS(.v12),
+        .visionOS(.v1)
+    ],
+    products: [
+        .library(
+            name: "FontKit",
+            targets: ["FontKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "FontKit",
+            resources: [.process("Fonts")]
+        ),
+        .testTarget(
+            name: "FontKitTests",
+            dependencies: ["FontKit"]
+        )
+    ]
+)
